a72961b073cf922d944245fffb276181ea575eb4
Kayo <91280312+okayosilva@users.noreply.github.com>
2025-02-18 12:12:36 -0300

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./components/header";
+import { Tasks } from "./components/tasks";
+
+function App() {
+  return (
+    <>
+      <Header>
+        <h1>Minha lista de tarefas</h1>
+      </Header>
+      <Tasks />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/tasks.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export const Tasks = () => {
+  const [taskList] = useState(["Trabalhar", "Estudar React"]);
+
+  return (
+    <div>
+      {taskList.length !== 0
+        ? taskList.map((task, index) => <p key={index}>{task}</p>)
+        : "Lista de tarefas vazia"}
+    </div>
+  );
+};
