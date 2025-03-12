d0693716976ce49a51ed9b52b597b666ab1bd91c
Kayo <91280312+okayosilva@users.noreply.github.com>
2025-03-12 12:52:23 -0300

    feat: list task

--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -1,13 +1,22 @@
+import { useState } from 'react';
+
 import PlusIcon from '../../assets/icons/add.svg?react';
 import CloudIcon from '../../assets/icons/cloud-sun.svg?react';
 import MoonIcon from '../../assets/icons/moon.svg?react';
 import SunIcon from '../../assets/icons/sun.svg?react';
 import TrashIcon from '../../assets/icons/trash.svg?react';
+import { TASKS } from '../../constants/tasks';
 import { Button } from '../Button';
 import { Header } from '../Header';
+import { TaskItem } from './taskItem';
 import { TaskSeparator } from './taskSeperator';
 
 export const Tasks = () => {
+  const [task] = useState(TASKS);
+
+  const morningTasks = task.filter((task) => task.time === 'morning');
+  const afternoonTasks = task.filter((task) => task.time === 'afternoon');
+  const eveningTasks = task.filter((task) => task.time === 'evening');
   return (
     <div className="w-full px-8 py-16">
       <div className="flex justify-between">
@@ -31,12 +40,21 @@ export const Tasks = () => {
       <div className="mt-6 flex flex-col gap-6 rounded-xl bg-white p-6">
         <div className="space-y-3">
           <TaskSeparator title="Manhã" icon={<SunIcon />} />
+          {morningTasks.map((task) => (
+            <TaskItem key={task.id} task={task} />
+          ))}
         </div>
         <div className="space-y-3">
           <TaskSeparator title="Tarde" icon={<CloudIcon />} />
+          {afternoonTasks.map((task) => (
+            <TaskItem key={task.id} task={task} />
+          ))}
         </div>
         <div className="space-y-3">
           <TaskSeparator title="Noite" icon={<MoonIcon />} />
+          {eveningTasks.map((task) => (
+            <TaskItem key={task.id} task={task} />
+          ))}
         </div>
       </div>
     </div>
