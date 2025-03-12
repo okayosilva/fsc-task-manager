fdc0cc61e9d3d0f31d0a30526f2b0af744d6c607
Kayo <91280312+okayosilva@users.noreply.github.com>
2025-03-12 14:16:18 -0300

    feat: adicionar ícones e checkbox ao item da tarefa

--- src/components/Tasks/taskItem.jsx
+++ src/components/Tasks/taskItem.jsx
@@ -1,11 +1,16 @@
+import CheckIcon from '../../assets/icons/check.svg?react';
+import DetailsIcon from '../../assets/icons/details.svg?react';
+import LoaderIcon from '../../assets/icons/loader-circle.svg?react';
+
 export const TaskItem = ({ task }) => {
   const { title, status } = task;
+  const checked = status === 'done';
+  const inProgress = status === 'in_progress';
 
   const getStatusClasses = () => {
-    if (status === 'done') return 'bg-[#00ADB5] bg-opacity-10 text-[#00ADB5]';
+    if (status === 'done') return 'bg-[#00ADB5] text-[#00ADB5]';
 
-    if (status === 'in_progress')
-      return 'bg-[#FFAA04] bg-opacity-10 text-[#FFAA04]';
+    if (status === 'in_progress') return 'bg-[#FFAA04]  text-[#FFAA04]';
 
     if (status === 'not_started')
       return 'bg-[#35383E] bg-opacity-10 text-[#35383E]';
@@ -13,9 +18,25 @@ export const TaskItem = ({ task }) => {
 
   return (
     <div
-      className={`flex items-center gap-2 rounded-lg px-4 py-3 text-sm ${getStatusClasses()}`}
+      className={`flex items-center rounded-lg bg-opacity-10 px-4 py-3 text-sm ${getStatusClasses()} justify-between`}
     >
-      {title}
+      <div className="flex items-center gap-2">
+        <label
+          className={`relative flex h-7 w-7 cursor-pointer items-center justify-center rounded-lg ${getStatusClasses()}`}
+        >
+          <input
+            type="checkbox"
+            checked={checked}
+            className="absolute h-full w-full cursor-pointer opacity-0"
+          />
+          {checked && <CheckIcon />}
+          {inProgress && <LoaderIcon className="animate-spin" />}
+        </label>
+        {title}
+      </div>
+      <a href="#" className="transition-all hover:opacity-75">
+        <DetailsIcon />
+      </a>
     </div>
   );
 };
