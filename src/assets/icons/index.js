a1fcda071b10efe580200eb24571e0606fc52df9
Kayo <91280312+okayosilva@users.noreply.github.com>
2025-03-17 13:39:21 -0300

    refactor: rename path imports

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,13 +1,13 @@
-import PlusIcon from './assets/icons/add.svg?react';
-import CheckIcon from './assets/icons/check.svg?react';
-import CloudIcon from './assets/icons/cloud-sun.svg?react';
-import DetailsIcon from './assets/icons/details.svg?react';
-import HomeIcon from './assets/icons/home.svg?react';
-import LoaderIcon from './assets/icons/loader-circle.svg?react';
-import MoonIcon from './assets/icons/moon.svg?react';
-import SunIcon from './assets/icons/sun.svg?react';
-import TasksIcon from './assets/icons/tasks.svg?react';
-import TrashIcon from './assets/icons/trash.svg?react';
+import PlusIcon from './add.svg?react';
+import CheckIcon from './check.svg?react';
+import CloudIcon from './cloud-sun.svg?react';
+import DetailsIcon from './details.svg?react';
+import HomeIcon from './home.svg?react';
+import LoaderIcon from './loader-circle.svg?react';
+import MoonIcon from './moon.svg?react';
+import SunIcon from './sun.svg?react';
+import TasksIcon from './tasks.svg?react';
+import TrashIcon from './trash.svg?react';
 
 export {
   CheckIcon,
